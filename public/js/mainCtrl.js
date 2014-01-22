9879123ae3201ff3d2b0601126c430a05e978c46
yhnavein <floubadour@gmail.com>
2014-01-22 23:09:32 +0100

    added additional assets, improved design
    coding basic angular models

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -1,6 +1,16 @@
+/*global console:false, angular:false */
 var app = angular.module('delegations', []);
 
 app.controller('mainCtrl', function($scope, $http) {
   var self = this;
 
+  $scope.transportWays = [ 'Train', 'Bus', 'Flight' ];
+
+  $http({
+      method: 'GET',
+      url: '/countries.json'
+    }).
+      success(function(data) {
+        $scope.countriesList = angular.fromJson(data);
+    });
 });
