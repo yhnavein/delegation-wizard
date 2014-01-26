b9871fa22eb4a9971e8f8f9b77443075ff7ab289
yhnavein <floubadour@gmail.com>
2014-01-26 17:15:08 +0100

    added some improvements for counting delegation days

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -15,27 +15,42 @@ app.controller('mainCtrl', function($scope, $http) {
     });
 
 
-  self.fullDateDiff = function(date1, date2) {
+  self.daysDiff = function(date1, date2) {
+    var fHalfDays = 0, fOneThirdDays = 0;
     var fullDate1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
     var fullDate2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
 
     if(fullDate2 === fullDate1) {
-      var hours = Math.floor( Math.abs( date2 - date1 ) / (1000 * 60 * 60) );
+      var hours = Math.abs( date2 - date1 ) / (1000 * 60 * 60);
 
-      return (hours >= 12 ? 1 : 0);
+      return {
+        fullDays : (hours > 12 ? 1 : 0),
+        halfDays : (hours <= 12 && hours > 8 ? 1 : 0),
+        oneThirdDays : (hours <= 8 ? 1 : 0)
+      };
     }
 
     var days = Math.floor( Math.abs( fullDate2 - fullDate1 ) / (1000 * 60 * 60 * 24) ) + 1;
 
     var firstDayHours = (24 - Math.floor( Math.abs( date1 - new Date(date1.getFullYear(), date1.getMonth(), date1.getDate()) ) / (1000 * 60 * 60) ));
-    if(firstDayHours < 12)
+    if(firstDayHours < 12) {
       days--;
+      fHalfDays += (firstDayHours > 8 ? 1 : 0);
+      fOneThirdDays += (firstDayHours <= 8 ? 1 : 0);
+    }
 
     var lastDayHours = Math.floor( Math.abs( date2 - new Date(date2.getFullYear(), date2.getMonth(), date2.getDate()) ) / (1000 * 60 * 60) );
-    if(lastDayHours < 12 && lastDayHours > 0)
+    if(lastDayHours < 12 && lastDayHours > 0) {
       days--;
+      fHalfDays += (lastDayHours > 8 ? 1 : 0);
+      fOneThirdDays += (lastDayHours <= 8 ? 1 : 0);
+    }
 
-    return days;
+    return {
+      fullDays : days,
+      halfDays : fHalfDays,
+      oneThirdDays : fOneThirdDays
+    };
   };
 
 
@@ -43,8 +58,7 @@ app.controller('mainCtrl', function($scope, $http) {
     if(typeof $scope.dateTo === 'undefined' || typeof $scope.dateFrom === 'undefined')
       return;
 
-    var fullDays = self.fullDateDiff($scope.dateFrom, $scope.dateTo);
+    $scope.days = self.daysDiff($scope.dateFrom, $scope.dateTo);
 
-    console.log('days: ' + datediff);
   };
 });
--- test/js/mainCtrl.tests.js
+++ test/js/mainCtrl.tests.js
@@ -1,3 +1,4 @@
+/*global describe:false, angular:false, expect:false, it:false, beforeEach:false, module:false, inject:false */
 'use strict';
 
 describe('delegations main controller', function(){
@@ -16,54 +17,83 @@ describe('delegations main controller', function(){
 
     // date diffing tests
 
-    it('full days diff between 2013-01-02 and 2013-01-05 is 4', function() {
-        var days = ctrl.fullDateDiff(new Date(2013,0,2), new Date(2013,0,5));
-        expect(days).toEqual(4);
+    it('days diff between 2013-01-02 and 2013-01-05 is 4', function() {
+        var days = ctrl.daysDiff(new Date(2013,0,2), new Date(2013,0,5));
+        expect(days.fullDays).toEqual(4);
     });
 
-    it('full days diff between 2013-01-02 and 2013-01-02 is 0', function() {
-        var days = ctrl.fullDateDiff(new Date(2013,0,2), new Date(2013,0,2));
-        expect(days).toEqual(0);
+    it('days diff between 2013-01-02 and 2013-01-02 is 0', function() {
+        var days = ctrl.daysDiff(new Date(2013,0,2), new Date(2013,0,2));
+        expect(days.fullDays).toEqual(0);
+        expect(days.halfDays).toEqual(0);
+        expect(days.oneThirdDays).toEqual(1);
     });
 
-    it('full days diff between 2013-01-02 10:00 and 2013-01-02 23:00 is 1', function() {
-        var days = ctrl.fullDateDiff(new Date(2013,0,2,10,0,0), new Date(2013,0,2,23,0,0));
-        expect(days).toEqual(1);
+    it('days diff between 2013-01-02 10:00 and 2013-01-02 23:00 is 1', function() {
+        var days = ctrl.daysDiff(new Date(2013,0,2,10,0,0), new Date(2013,0,2,23,0,0));
+        expect(days.fullDays).toEqual(1);
     });
 
-    it('full days diff between 2013-01-02 10:30 and 2013-01-02 22:30 is 1', function() {
-        var days = ctrl.fullDateDiff(new Date(2013,0,2,10,30,0), new Date(2013,0,2,22,30,0));
-        expect(days).toEqual(1);
+    it('days diff between 2013-01-02 10:30 and 2013-01-02 22:30 is 0', function() {
+        var days = ctrl.daysDiff(new Date(2013,0,2,10,30,0), new Date(2013,0,2,22,30,0));
+        expect(days.fullDays).toEqual(0);
+        expect(days.halfDays).toEqual(1);
     });
 
-    it('full days diff between 2013-01-02 10:30 and 2013-01-02 22:29 is 0', function() {
-        var days = ctrl.fullDateDiff(new Date(2013,0,2,10,30,0), new Date(2013,0,2,22,29,0));
-        expect(days).toEqual(0);
+    it('days diff between 2013-01-02 10:30 and 2013-01-02 22:31 is 1', function() {
+        var days = ctrl.daysDiff(new Date(2013,0,2,10,30,0), new Date(2013,0,2,22,31,0));
+        expect(days.fullDays).toEqual(1);
+        expect(days.halfDays).toEqual(0);
     });
 
-    it('full days diff between 2013-01-02 10:00 and 2013-01-02 15:00 is 0', function() {
-        var days = ctrl.fullDateDiff(new Date(2013,0,2,10,0,0), new Date(2013,0,2,15,0,0));
-        expect(days).toEqual(0);
+    it('days diff between 2013-01-02 10:30 and 2013-01-02 22:29 is 0', function() {
+        var days = ctrl.daysDiff(new Date(2013,0,2,10,30,0), new Date(2013,0,2,22,29,0));
+        expect(days.fullDays).toEqual(0);
+        expect(days.halfDays).toEqual(1);
     });
 
-    it('full days diff between 2013-01-02 and 2013-02-02 is 32', function() {
-        var days = ctrl.fullDateDiff(new Date(2013,0,2), new Date(2013,1,2));
-        expect(days).toEqual(32);
+    it('days diff between 2013-01-02 10:00 and 2013-01-02 15:00 is 0', function() {
+        var days = ctrl.daysDiff(new Date(2013,0,2,10,0,0), new Date(2013,0,2,15,0,0));
+        expect(days.fullDays).toEqual(0);
+        expect(days.oneThirdDays).toEqual(1);
     });
 
-    it('full days diff between 2013-01-02 10:00 and 2013-01-02 15:00 is 0', function() {
-        var days = ctrl.fullDateDiff(new Date(2013,0,2,10,0,0), new Date(2013,0,2,15,0,0));
-        expect(days).toEqual(0);
+    it('days diff between 2013-01-02 and 2013-02-02 is 32', function() {
+        var days = ctrl.daysDiff(new Date(2013,0,2), new Date(2013,1,2));
+        expect(days.fullDays).toEqual(32);
     });
 
-    it('full days diff between 2013-01-02 10:00 and 2013-01-03 9:00 is 1', function() {
-        var days = ctrl.fullDateDiff(new Date(2013,0,2,10,0,0), new Date(2013,0,3,9,0,0));
-        expect(days).toEqual(1);
+    it('days diff between 2013-01-02 10:00 and 2013-01-02 15:00 is 0', function() {
+        var days = ctrl.daysDiff(new Date(2013,0,2,10,0,0), new Date(2013,0,2,15,0,0));
+        expect(days.fullDays).toEqual(0);
+        expect(days.halfDays).toEqual(0);
+        expect(days.oneThirdDays).toEqual(1);
     });
 
-    it('full days diff between 2013-01-02 10:00 and 2013-01-06 18:30 is 3', function() {
-        var days = ctrl.fullDateDiff(new Date(2013,0,2,10,0,0), new Date(2013,0,6,18,30,0));
-        expect(days).toEqual(5);
+    it('days diff between 2013-01-02 10:00 and 2013-01-03 9:00 is 1', function() {
+        var days = ctrl.daysDiff(new Date(2013,0,2,10,0,0), new Date(2013,0,3,9,0,0));
+        expect(days.fullDays).toEqual(1);
+        expect(days.halfDays).toEqual(1);
+        expect(days.oneThirdDays).toEqual(0);
+    });
+
+    it('days diff between 2013-01-02 19:00 and 2013-01-03 9:00 is 0', function() {
+        var days = ctrl.daysDiff(new Date(2013,0,2,19,0,0), new Date(2013,0,3,9,0,0));
+        expect(days.fullDays).toEqual(0);
+        expect(days.halfDays).toEqual(1);
+        expect(days.oneThirdDays).toEqual(1);
+    });
+
+    it('days diff between 2013-01-02 10:00 and 2013-01-06 18:30 is 5', function() {
+        var days = ctrl.daysDiff(new Date(2013,0,2,10,0,0), new Date(2013,0,6,18,30,0));
+        expect(days.fullDays).toEqual(5);
+    });
+
+    it('days diff between 2013-01-02 19:00 and 2013-01-06 11:30 is 3', function() {
+        var days = ctrl.daysDiff(new Date(2013,0,2,19,0,0), new Date(2013,0,6,11,30,0));
+        expect(days.fullDays).toEqual(3);
+        expect(days.halfDays).toEqual(1);
+        expect(days.oneThirdDays).toEqual(1);
     });
 
 });
