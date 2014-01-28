d917133bf6b8db64860fe241aab7724c00b35a44
Piotrek Dąbrowski <admin@puredev.eu>
2014-01-28 13:51:49 +0100

    added delegation details section

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -52,6 +52,30 @@ app.controller('mainCtrl', function($scope, $http) {
     };
   };
 
+  self.prepareDelegationDays = function(from, to) {
+    var days = [];
+    var iterator = new Date(from.getFullYear(), from.getMonth(), from.getDate());
+
+    if (to <= from)
+      return days;
+
+    for(;;) {
+      days.push({
+        date: iterator.toLocaleDateString(),
+        dayType: 1,
+        provBreakfast : false,
+        provDinner : false,
+        provSupper : false,
+        excluded : false
+      });
+      if(iterator >= to)
+        break;
+      iterator.setDate(iterator.getDate() + 1);
+    }
+
+    return days;
+  };
+
   $scope.delegationCost = function() {
     if(typeof $scope.dateTo === 'undefined' || typeof $scope.dateFrom === 'undefined' || typeof $scope.country === 'undefined')
       return 0;
@@ -59,10 +83,11 @@ app.controller('mainCtrl', function($scope, $http) {
     return $scope.country.diem * ($scope.days.fullDays + $scope.days.halfDays / 2 + $scope.days.oneThirdDays / 3);
   };
 
-  $scope.datesChange = function() {
+  $scope.datesChange = function(f) {
     if(typeof $scope.dateTo === 'undefined' || typeof $scope.dateFrom === 'undefined')
       return;
 
     $scope.days = self.daysDiff($scope.dateFrom, $scope.dateTo);
+    $scope.delegationDays = self.prepareDelegationDays($scope.dateFrom, $scope.dateTo);
   };
 });
