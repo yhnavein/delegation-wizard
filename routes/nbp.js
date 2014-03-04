6c42b7feca2ed4e029eef8dbcff601bf07ef23f9
Piotrek Dąbrowski <admin@puredev.eu>
2014-03-04 17:04:17 +0100

    trying to integrate mongodb to store rates
    experimenting... it needs a lot of refactoring

--- routes/nbp.js
+++ routes/nbp.js
@@ -3,6 +3,7 @@ var http = require("http"),
     zlib = require("zlib"),
     sugar = require("sugar"),
     nbpDirCache = require("../nbpDirCache")(),
+    mongojs = require('mongojs'),
     xml2json = require("xml2json");
 
 var options = {
@@ -16,6 +17,9 @@ var options = {
     'Accept-Language': 'en-US,en;q=0.8'
   }
 };
+
+var db = mongojs('mongodb://localhost/test', ['nbpRates']);
+var exchangeRates = db.nbpRates;
 
 var makeRequest = function(path, success) {
   options.path = path;
@@ -78,38 +82,60 @@ var fixMoneyValue = function(value) {
   return parseFloat(value);
 };
 
+var processExchangeRate = function(ratesJson, currency, res) {
+  var reqCurrency = ratesJson.tabela_kursow.pozycja.find(function(el) {
+    return el.kod_waluty === currency;
+  });
+
+  if(typeof reqCurrency === 'undefined')
+    res.send(400, 'Unknown currency!');
+  else
+    res.send({
+      submitDate: ratesJson.tabela_kursow.data_publikacji,
+      multiplier: reqCurrency.przelicznik,
+      averageRate: fixMoneyValue(reqCurrency.kurs_sredni),
+      currencyCode: reqCurrency.kod_waluty
+    });
+
+  res.end();
+};
+
 exports.getPLNRate = function(req, res){
   var currency = req.query.currency;
   var day = moment(req.query.date); //date when document will be submitted
   var nbpDate = parseInt(day.format("YYMMDD"), 10);
 
-  makeDirRequest(function(data) {
-    var tableName = findProperExchangeRateTable(data, nbpDate);
-    if(tableName === null || typeof tableName === 'undefined'){
-      res.send(400, 'We can\'t find proper exchange rates!');
-      res.end();
+  exchangeRates.findOne({date: day.format("YYMMDD")}, function(err, data) {
+    console.log('Err: ' + err);
+    console.log('Data: ' + data);
+
+    if( err || !data)
+      console.log("No records returned!");
+    else {
+      processExchangeRate(data.json, currency, res);
+      return;
     }
 
-    var url = '/kursy/xml/' + tableName.trim() + '.xml';
-    makeRequest(url, function(ratesXml) {
-      var ratesJson = JSON.parse( xml2json.toJson(ratesXml) );
+    makeDirRequest(function(data) {
+      var tableName = findProperExchangeRateTable(data, nbpDate);
+      if(tableName === null || typeof tableName === 'undefined'){
+        res.send(400, 'We can\'t find proper exchange rates!');
+        res.end();
+      }
 
-      var reqCurrency = ratesJson.tabela_kursow.pozycja.find(function(el) {
-        return el.kod_waluty === currency;
-      });
-
-      if(typeof reqCurrency === 'undefined')
-        res.send(400, 'Unknown currency!');
-      else
-        res.send({
-          submitDate: ratesJson.tabela_kursow.data_publikacji,
-          multiplier: reqCurrency.przelicznik,
-          averageRate: fixMoneyValue(reqCurrency.kurs_sredni),
-          currencyCode: reqCurrency.kod_waluty
+      var url = '/kursy/xml/' + tableName.trim() + '.xml';
+      makeRequest(url, function(ratesXml) {
+        var ratesJson = JSON.parse( xml2json.toJson(ratesXml) );
+        exchangeRates.save({
+          json: ratesJson,
+          date: day.format("YYMMDD")
+        }, function(err, saved) {
+          console.log('ratesJson: ' + ratesJson);
+          processExchangeRate(ratesJson, currency, res);
         });
-
-      res.end();
+      });
     });
+
   });
 
 };
