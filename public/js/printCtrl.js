5ce72864aeac3a1028b898630d4f7f1a2234fbd8
Piotrek Dąbrowski <floubadour@gmail.com>
2014-07-16 11:03:06 +0200

    fixed module configuration
    to make test run properly

--- public/js/printCtrl.js
+++ public/js/printCtrl.js
@@ -1,5 +1,5 @@
 /*global console:false, angular:false */
-var app = angular.module('delegations', ['mgcrea.ngStrap']);
+var app = angular.module('delegations-print', ['mgcrea.ngStrap']);
 
 app.controller('printCtrl', function($scope) {
   var self = this;
